7a75e8f1ecabfaba3d48f1e76b1384af18a961ad
europa1610 <afzalahmad0203@gmail.com>
2022-12-30 14:47:54 +0800

    Very nice

--- trainval/trainval_tpu.py
+++ trainval/trainval_tpu.py
@@ -11,6 +11,7 @@ import torch_xla.core.xla_model as xm
 import auxiliary.utils as utils
 
 from models.accelbenchnet import AccelNet as Network
+import torch_xla.distributed.parallel_loader as pl
 
 
 def _train_update(device, step, loss, tracker, epoch, writer):
